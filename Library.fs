144e6a73f49cac61f24ff3953655d52cdeb3a189
Matt Bayliss <github@mattbayliss.com>
2020-01-27 23:54:31 +1100

    Initial commit - just playing yo

---
+++ Library.fs
@@ -0,0 +1,31 @@
+﻿namespace MeanderingHeroes
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
+module Engine =
+    type Point = {
+        x : double;
+        y : double;
+    }
+
+    type Agent = {
+        id  : int32;
+        name: string;
+        position: Point;
+        health: single;
+    }
+
+    type WorldState = {
+        agents : Agent list;
+        tick : int32;
+    }
+
+    type Instruction = {
+        agentId : int32;
+        command : string;
+    }
+
+    let WorldTick (currentState : WorldState) (instructions : Instruction list) = 
+        "helllo"
