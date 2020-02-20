9ef36c33e715812a3eeeb5f6f9d995dfcd41f655
Matt Bayliss <github@mattbayliss.com>
2020-02-21 02:30:13 +1100

    tests working in fsi

--- src/UnitTests/Program.fs
+++ src/UnitTests/Program.fs
@@ -1,9 +1,20 @@
-﻿open System
-open Fuchu
+﻿open Fuchu
+open MeanderingHeroes.Engine
 
-let simpleTest = 
-    testCase "A simple test" <|
-        fun _ -> Assert.Equal("2+2", 4, 2+2)
+let testGrid = 
+    let intgrid = seq { 1 .. 10 }
+    seq { 1 .. 10 } |> Seq.map (
+        fun x -> 
+            seq { 1 .. 10 } |> Seq.map (fun y -> { coords = { x = double x; y = double y }; terrain = Terrain.Grass })
+        )
+
+let tests =
+    testList "Move Test Group" [
+        testCase "one test" <|
+            fun _ -> Assert.Equal("2+2", 4, 2+2)
+        testCase "another test" <|
+            fun _ -> Assert.Equal("3+3", 6, 3+3)
+    ]
 
 [<EntryPoint>]
 let main args = defaultMainThisAssembly args
