eea4028a9f8d394d3113ecd52d9be7d839fc0686
Matt Bayliss <github@mattbayliss.com>
2020-02-23 19:12:45 +1100

    wip - thinking about observables

--- src/HeroEngine/Library.fs
+++ src/HeroEngine/Library.fs
@@ -1,9 +1,5 @@
 ﻿namespace MeanderingHeroes
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
-
 module Engine =
     type Point = {
         x : double;
@@ -14,7 +10,8 @@ module Engine =
         id  : int32;
         name: string;
         position: Point;
-        health: single;        
+        health: single;
+        speed: single;
     }
 
     type Terrain = Grass=0 | Forest=1
@@ -22,13 +19,13 @@ module Engine =
     type GridSquare = {
         coords : Point; // squares will be in a 2 dimension array, so this is kind of duplicated info, but will be used to calculate distances?
         terrain : Terrain;
-        // temperature could be a function of coords, day of the year (WordState.tick), and a broader weather system function?
     }
+    // temperature could be a function of coords, day of the year (WordState.tick), and a broader weather system function?
 
     type WorldState = {
         agents : Agent list;
         tick : int32;
-        mapGrid : seq<seq<GridSquare>>;
+        mapGrid : GridSquare[,];
     }
 
     type Instruction = {
@@ -36,7 +33,29 @@ module Engine =
         command : string;
     }
 
-    let WorldTick (currentState : WorldState) (instructions : Instruction list) = 
-        "helllo"
+    type Event = {
+        agentId : int32;
+        from: Point;
+        dest: Point;
+    }
 
+    let terrainMovementCost terrain =
+        match terrain with
+            | Terrain.Grass -> 1.0F
+            | Terrain.Forest -> 1.5F
+            | _ -> 2.0F
     
+    let distance (a, b):double = 
+        sqrt (a*a + b*b)    
+
+    let moveInstruction grid agent target =
+        let (x, y) = (target.x - agent.position.x, target.y - agent.position.y)
+        let d = distance (x, y)
+        // identity vector - direction we need to travel, with a length of 1
+        // (one day we'll actually need to find the best path)
+        let (ix, iy) = (x/d, y/d)
+        { id = agent.id; name = agent.name; position = {x = ix * (double agent.speed); y = iy * (double agent.speed)}; speed = agent.speed; health = agent.health }     
+
+    let WorldTick (currentState : WorldState) (instructions : Instruction list) = 
+        { agents = currentState.agents; tick = currentState.tick + 1; mapGrid = currentState.mapGrid }, 
+
--- src/UnitTests/Program.fs
+++ src/UnitTests/Program.fs
@@ -2,16 +2,25 @@
 open MeanderingHeroes.Engine
 
 let testGrid = 
-    let intgrid = seq { 1 .. 10 }
-    seq { 1 .. 10 } |> Seq.map (
-        fun x -> 
-            seq { 1 .. 10 } |> Seq.map (fun y -> { coords = { x = double x; y = double y }; terrain = Terrain.Grass })
-        )
+    Array2D.init 10 10 (fun x y ->{ coords = { x = double (x + 1); y = double (y + 1) }; terrain = Terrain.Grass } )
+
+let initialTestWorldState = { agents = List.Empty; tick = 1; mapGrid = testGrid }
 
 let tests =
     testList "Move Test Group" [
-        testCase "one test" <|
-            fun _ -> Assert.Equal("2+2", 4, 2+2)
+        testCase "move east" <|
+            fun _ ->
+                // simulate the ticks we need until target is reached
+                let mover = { id = 1; name = "Sally Walker"; position = { x = 1.0; y = 1.0 }; health = 1.0F; speed = 1.0F }; 
+                let instruction = setDestination mover (10.0, 1.0)
+                let moveWorldState = { agents = [ mover ]; tick = 1; mapGrid = testGrid };
+
+                // tick 1
+                let state1 = MeanderingHeroes.Engine.WorldTick moveWorldState [ instruction ]
+
+                
+                
+                Assert.Equal("2+2", 4, 2+2)
         testCase "another test" <|
             fun _ -> Assert.Equal("3+3", 6, 3+3)
     ]
